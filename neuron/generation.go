47aa211307d639379de8a1055da90c8665409c2c
Sammy Farid <sammy@sammys-air.lan>
2020-10-06 00:59:46 -0700

    competition outline, closer but need to stop for the night

--- neuron/generation.go
+++ neuron/generation.go
@@ -1,10 +1,8 @@
 package neuron
 
-// AccuracyFunc scores the output outputs. Closer to 0 is better.
-type AccuracyFunc func(inputs []SignalType, outputs []SignalType) int
-
 type BrainResult struct {
 	id      IDType
+	inputs  []SignalType
 	outputs []SignalType
 	steps   int
 }
@@ -15,16 +13,13 @@ type GenerationConfig struct {
 
 type Generation struct {
 	config GenerationConfig
-
-	brains  map[IDType]*Brain
-	resChan chan BrainResult
+	brains map[IDType]*Brain
 }
 
-func NewGeneration(gconf GenerationConfig, codes map[IDType]*DNA, resChan chan BrainResult) *Generation {
+func NewGeneration(gconf GenerationConfig, codes map[IDType]*DNA) *Generation {
 	g := &Generation{
-		config:  gconf,
-		brains:  make(map[IDType]*Brain, len(codes)),
-		resChan: resChan,
+		config: gconf,
+		brains: make(map[IDType]*Brain, len(codes)),
 	}
 
 	for id, dna := range codes {
@@ -33,11 +28,11 @@ func NewGeneration(gconf GenerationConfig, codes map[IDType]*DNA, resChan chan B
 	return g
 }
 
-func (g *Generation) FireBrains(inputs []SignalType) map[IDType]*BrainResult {
+func (g *Generation) FireBrains(inputs map[IDType][]SignalType) map[IDType]*BrainResult {
 	// Simulate all brains in separate goroutines.
 	resChan := make(chan BrainResult)
 	for id := range g.brains {
-		go g.fireBrain(id, inputs)
+		go g.fireBrain(id, inputs[id], resChan)
 	}
 
 	// Wait for all the results to come in before returning.
@@ -50,79 +45,26 @@ func (g *Generation) FireBrains(inputs []SignalType) map[IDType]*BrainResult {
 	return results
 }
 
-func (g *Generation) fireBrain(id IDType, inputs []SignalType) {
+func (g *Generation) fireBrain(id IDType, inputs []SignalType, resChan chan BrainResult) {
 	brain := g.brains[id]
 	brain.SeeInput(inputs)
+
+	result := BrainResult{
+		id:     id,
+		inputs: inputs,
+	}
 
 	for step := 0; step < g.config.MaxSteps; step++ {
 		outputs := brain.StepFunction()
 		if len(outputs) > 0 {
-			g.resChan <- BrainResult{
-				id:      id,
-				outputs: outputs,
-				steps:   step,
-			}
+			result.outputs = outputs
+			result.steps = step
+			resChan <- result
 			return
 		}
 	}
 
-	g.resChan <- BrainResult{
-		id:      id,
-		outputs: make([]SignalType, 0),
-		steps:   g.config.MaxSteps,
-	}
-}
-
-func RunGeneration(codes map[IDType]*DNA, envInputs []SignalType, maxSteps int) map[int]*BrainResult {
-	// Simulate all brains in separate goroutines.
-	resChan := make(chan BrainResult)
-	for id, code := range codes {
-		go FireBrain(id, code, envInputs, maxSteps, resChan)
-	}
-
-	// Wait for all the scores to come in.
-	results := make(map[int]*BrainResult, len(codes))
-	for i := 0; i < len(codes); i++ {
-		result := <-resChan
-		results[result.id] = &result
-	}
-
-	return results
-}
-
-func FireBrain(id IDType, dna *DNA, envInputs []SignalType, maxSteps int, resChan chan BrainResult) {
-	brain := Flourish(dna)
-
-	// At some point it would be good to add support for
-	// multiple rounds of seeing. Maybe [][]SignalType
-	brain.SeeInput(envInputs)
-	for step := 0; step < maxSteps; step++ {
-		outputs := brain.StepFunction()
-		if len(outputs) > 0 {
-			resChan <- BrainResult{
-				id:      id,
-				outputs: outputs,
-				steps:   step,
-			}
-			return
-		}
-	}
-
-	resChan <- BrainResult{
-		id:      id,
-		outputs: make([]SignalType, 0),
-		steps:   maxSteps,
-	}
-}
-
-func FireBrainBlock(dna *DNA, envInputs []SignalType, maxSteps int) []SignalType {
-	brain := Flourish(dna)
-	brain.SeeInput(envInputs)
-	for step := 0; step < maxSteps; step++ {
-		outputs := brain.StepFunction()
-		if len(outputs) > 0 {
-			return outputs
-		}
-	}
-	return make([]SignalType, 0)
+	result.outputs = make([]SignalType, 0)
+	result.steps = g.config.MaxSteps
+	resChan <- result
 }
--- neuron/playground.go
+++ neuron/playground.go
@@ -3,27 +3,40 @@ package neuron
 import (
 	"fmt"
 	"log"
-	"math"
 	"math/rand"
 	"sort"
 	"time"
 )
 
+// FitnessFunc scores the output outputs. Closer to 0 is better.
+type FitnessFunc func(inputs []SignalType, outputs []SignalType) int
+
 type PlaygroundConfig struct {
-	// Playground
-	NumSpecies       int
-	MaxGensPerPlay   int
+	// Running the playground
+	NumSpecies  int
+	Generations int
+
+	// Competition
+	RoundsOfCompetition int // How many times to play against another species.
+	NumCompetitions     int // How many other species to play against.
+
+	// Evolution
+	SpeciesReproduce int // How many species reproduce (and die off).
+
+	// Creating species
 	DnaSeedSnippets  int
 	DnaSeedMutations int
 
-	gconf GenerationConfig
+	// Scoring
+	FitnessFn FitnessFunc
 
-	AccuracyFn AccuracyFunc
+	// Nested configs
+	Gconf GenerationConfig
 }
 
 type Playground struct {
-	codes  map[IDType]*DNA
 	config PlaygroundConfig
+	codes  map[IDType]*DNA
 	rnd    *rand.Rand
 }
 
@@ -33,12 +46,11 @@ type SpeciesScore struct {
 }
 
 func NewPlayground(config PlaygroundConfig) *Playground {
-	p := Playground{
-		codes:  make(map[int]*DNA),
+	return &Playground{
 		config: config,
+		codes:  make(map[IDType]*DNA),
 		rnd:    rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
-	return &p
 }
 
 func (p *Playground) SeedRandDNA() {
@@ -48,14 +60,68 @@ func (p *Playground) SeedRandDNA() {
 	}
 }
 
-func (p *Playground) SeedKnownDNA(dna *DNA) {
-	for id := 0; id < p.config.NumSpecies; id++ {
-		p.codes[id] = dna.DeepCopy()
+func (p *Playground) SimulateCompetition(envInputs []SignalType) {
+	// Each generation gets the set of inputs, competes, reproduces, and mutates.
+	for gen := 0; gen < p.config.Generations; gen++ {
+		g := NewGeneration(p.config.Gconf, p.codes)
+
+		// Send the environment inputs to every species to begin.
+		inputs := make(map[IDType][]SignalType, p.config.NumSpecies)
+		for id := range p.codes {
+			inputs[id] = envInputs
+		}
+		results := g.FireBrains(inputs)
+
+		scores := make([]SpeciesScore, len(results))
+
+		// Each competition pits two species against each other.
+		// for comp := 0; comp < p.config.NumCompetitions; comp++ {
+		for i := 0; i < p.config.NumSpecies; i++ {
+			for j := i + 1; j < p.config.NumSpecies; j++ {
+				// PICK UP HERE
+				// To do every combination of brains, it seems like it makes more sense
+				// to do `go FireBrain` and then have it write the results
+				// It should be a lot easier here to have a struct (or add to playground)
+				// with a mutex and have functions like
+				// GetNextInput(id) which waits on the other brain
+				// try using thread safe structs instead of passing channels
+			}
+		}
+
+		for round := 0; round < p.config.RoundsOfCompetition; round++ {
+
+			results = g.FireBrains(inputs)
+			for id, result := range results {
+				scores[id].score += p.config.FitnessFn(result.inputs, result.outputs)
+			}
+		}
+		// }
+
+		// Sorts low to high (lower scores are better).
+		sort.Slice(scores, func(i, j int) bool {
+			return scores[i].score < scores[j].score
+		})
+
+		fmt.Printf("Scores for gen %d: Min=%d 25th=%d 50th=%d 75th=%d Max=%d\n", gen,
+			scores[0].score, scores[len(scores)/4].score, scores[2*len(scores)/4].score,
+			scores[3*len(scores)/4].score, scores[len(scores)-1].score)
+
+		for i := 0; i < p.config.SpeciesReproduce; i++ {
+			winnerID := scores[i].id
+			idToReplace := scores[p.config.NumSpecies-i-1].id
+
+			// Create a copy of the underlying DNA struct to have different references
+			// at each index even though the (pointer to the) source DNA is the same.
+			p.codes[idToReplace] = p.codes[winnerID].DeepCopy()
+			p.mutateDNA(p.codes[idToReplace])
+		}
 	}
+
 }
 
-// Run an evolution and return the best DNA after n generations.
-func (p *Playground) SimulatePlayground(envInputs []SignalType) *DNA {
+/*
+// SimulatePlayground runs a full generation of DNA.
+func (p *Playground) SimulatePlayground(envInputs []SignalType) {
 	fmt.Printf("Beginning evolution with input %v\n", envInputs)
 	for i := 0; i < p.config.MaxGensPerPlay; i++ {
 		for id, code := range p.codes {
@@ -95,8 +161,6 @@ func (p *Playground) SimulatePlayground(envInputs []SignalType) *DNA {
 		// break
 		// }
 	}
-
-	return p.codes[0].DeepCopy()
 }
 
 func (p *Playground) setNextGenCodes(scores []SpeciesScore) {
@@ -105,7 +169,9 @@ func (p *Playground) setNextGenCodes(scores []SpeciesScore) {
 		return scores[i].score < scores[j].score
 	})
 
-	fmt.Printf("Min=%d 25th=%d 50th=%d 75th=%d Max=%d\n", scores[0].score, scores[len(scores)/4].score, scores[2*len(scores)/4].score, scores[3*len(scores)/4].score, scores[len(scores)-1].score)
+	fmt.Printf("Min=%d 25th=%d 50th=%d 75th=%d Max=%d\n",
+		scores[0].score, scores[len(scores)/4].score, scores[2*len(scores)/4].score,
+		scores[3*len(scores)/4].score, scores[len(scores)-1].score)
 
 	numToCopy := 2
 
@@ -123,7 +189,8 @@ func (p *Playground) setNextGenCodes(scores []SpeciesScore) {
 	p.codes = newCodes
 }
 
-func (p *Playground) scoreResult(id int, result *BrainResult, inputs []SignalType) SpeciesScore {
+func (p *Playground) scoreResult(
+	id int, result *BrainResult, inputs []SignalType) SpeciesScore {
 	// An accuracy score of 0 means this is the ideal output.
 	// Prioritize getting a high accuracy first before paring down.
 	// if accScore != 0 {
@@ -147,16 +214,18 @@ func dnaComplexity(dna *DNA) int {
 	}
 	return complexity
 }
+*/
 
 func (p *Playground) initRandDNA() *DNA {
 	dna := NewDNA()
 	for i := 0; i < p.config.DnaSeedSnippets; i++ {
 		dna.AddSnippet(p.rnd.Intn(NumOps))
 	}
+
 	// Just winging this.
-	dna.visionIDs = append(dna.visionIDs, 0)
-	dna.visionIDs = append(dna.visionIDs, 1)
-	dna.motorIDs = append(dna.visionIDs, p.config.DnaSeedSnippets-1)
+	dna.visionIDs.InsertID(0)
+	dna.visionIDs.InsertID(1)
+	dna.motorIDs.InsertID(p.config.DnaSeedSnippets - 1)
 
 	for i := 0; i < p.config.DnaSeedMutations; i++ {
 		p.mutateDNA(dna)
@@ -169,7 +238,7 @@ func (p *Playground) mutateDNA(dna *DNA) {
 	// 	dna.AddSnippet(p.rnd.Intn(NUM_OPS))
 	// }
 
-	for snipIndex, snip := range dna.snips {
+	for snipIndex, snip := range dna.snippets {
 		// if p.rnd.Float32() < 0.01 {
 		// 	dna.DeleteSnippet(snipIndex)
 		// 	continue
@@ -194,7 +263,7 @@ func (p *Playground) mutateDNA(dna *DNA) {
 		// }
 
 		if p.rnd.Float32() < 0.10 {
-			snip.SetOp(p.rnd.Intn(NUM_OPS))
+			snip.SetOp(p.rnd.Intn(NumOps))
 		}
 		// if p.rnd.Float32() < 0.10 {
 		// 	snip.AddSynapse(p.randSnippetId(dna))
@@ -219,9 +288,9 @@ func (p *Playground) mutateDNA(dna *DNA) {
 	}
 }
 
-func (p *Playground) randSnippetId(dna *DNA) int {
-	randIndex := p.rnd.Intn(len(dna.snips))
-	for snipID := range dna.snips {
+func (p *Playground) randSnippetId(dna *DNA) IDType {
+	randIndex := p.rnd.Intn(len(dna.snippets))
+	for snipID := range dna.snippets {
 		if randIndex == 0 {
 			return snipID
 		}
